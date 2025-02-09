b94dc0a1aeaebd5d62b75e31a6433ab1786be55a
BeroKiTeer <1377433202@qq.com>
2025-02-09 19:23:13 +0800

    基本的用户信息更新逻辑

--- app/user/biz/model/user.go
+++ app/user/biz/model/user.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"context"
+	"errors"
 	"gorm.io/gorm"
 )
 
@@ -28,4 +29,38 @@ func GetByEmail(db *gorm.DB, ctx context.Context, email string) (user *User, err
 func GetUserById(db *gorm.DB, ctx context.Context, id int32) (user *User, err error) {
 	err = db.WithContext(ctx).Where("id = ?", id).First(&user).Error
 	return
+}
+
+// UserExistsByID 检查用户是否存在
+func UserExistsByID(db *gorm.DB, userID int32) (bool, error) {
+	var exists bool
+	err := db.Raw("SELECT EXISTS(SELECT 1 FROM users WHERE id = ?)", userID).
+		Scan(&exists).
+		Error
+
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
+
+// UpdateUser 更新用户信息
+func UpdateUser(db *gorm.DB, ctx context.Context, userID int32, updates map[string]interface{}) error {
+	// 1️⃣ 检查用户是否存在
+	var user User
+	err := db.WithContext(ctx).Where("id = ?", userID).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.New("用户不存在")
+	}
+	if err != nil {
+		return err
+	}
+
+	// 2️⃣ 执行更新操作
+	err = db.WithContext(ctx).Model(&user).Updates(updates).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ app/user/biz/model/user_test.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"testing"
+)
+
+func TestUserDAO(t *testing.T) {
+	// todo: edit your unit test
+	//_, err := UserExistsByID(mysql.DB, 1)
+	//if err != nil {
+	//	return
+	//}
+}
