5d9f6fa9bab67dc6c1b693d52d6d1fb5c0a6e099
Muschuang123 <Muschuang123@163.com>
2025-02-10 21:36:18 +0800

    生成Token、验证Token、续期Token已实现，均通过测试。默认Token过期时间一小时，redis 地址 localhost:6379。

--- app/auth/biz/service/verify_token_by_rpc.go
+++ app/auth/biz/service/verify_token_by_rpc.go
@@ -3,12 +3,9 @@ package service
 import (
 	auth "auth/kitex_gen/auth"
 	"context"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"strconv"
-	"strings"
 )
 
 type VerifyTokenByRPCService struct {
@@ -21,25 +18,9 @@ func NewVerifyTokenByRPCService(ctx context.Context) *VerifyTokenByRPCService {
 // Run create note info
 func (s *VerifyTokenByRPCService) Run(req *auth.VerifyTokenReq) (resp *auth.VerifyResp, err error) {
 
-	// 把 token 分为好几段，其中第二段（parts[1]）是 payload
-	parts := strings.Split(req.Token, ".")
-	// 按照 base64 解码
-	payloadString, err := base64.RawURLEncoding.DecodeString(parts[1])
+	userID, err := GetUserIDFromToken(req.Token)
 	if err != nil {
-		return &auth.VerifyResp{Res: false}, err
-	}
-
-	// 把解码的结果 转成 JSON 的 map形式 拿到 user_id
-	var payloadJSON map[string]interface{}
-	err = json.Unmarshal(payloadString, &payloadJSON)
-	if err != nil {
-		return &auth.VerifyResp{Res: false}, err
-	}
-
-	// 断言 user_id 是 float64，稍后强转为 string
-	userID, ok := payloadJSON["user_id"].(float64)
-	if !ok {
-		return &auth.VerifyResp{Res: false}, fmt.Errorf("user_id 非法，token 无效。")
+		return nil, err
 	}
 
 	// 查询 userID 在 redis 里面是否存在，如果存在，获取密钥并验证
