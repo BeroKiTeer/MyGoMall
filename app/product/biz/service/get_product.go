00ff6cbe0e1920e44606dcfeb6de333714c333b8
赵宇锋 <zyf7312@qq.com>
2025-02-10 21:29:09 +0800

    通过id获取单个商品信息

--- app/product/biz/service/get_product.go
+++ app/product/biz/service/get_product.go
@@ -2,7 +2,12 @@ package service
 
 import (
 	"context"
-	product "product/kitex_gen/product"
+	"fmt"
+	"product/biz/dal"
+	"product/biz/dal/mysql"
+	"product/biz/model"
+	"product/kitex_gen/product"
+	"strconv"
 )
 
 type GetProductService struct {
@@ -14,7 +19,24 @@ func NewGetProductService(ctx context.Context) *GetProductService {
 
 // Run create note info
 func (s *GetProductService) Run(req *product.GetProductReq) (resp *product.GetProductResp, err error) {
-	// Finish your business logic.
-
-	return
+	fmt.Printf("请求id：%+v\n", req.GetId())
+	p := getProduct(int(req.GetId()))
+	resp = &product.GetProductResp{}
+	resp.Product = &product.Product{
+		Id:          uint32(p.Id),
+		Name:        p.Name,
+		Description: p.Description,
+		Picture:     p.Images,
+		Price:       p.Price,
+		Categories:  []string{},
+	}
+	return resp, nil
+}
+func getProduct(id int) model.Product {
+	dal.Init()
+	var row model.Product
+	mysql.DB.Model(&model.Product{}).Where("id=" + strconv.Itoa(id)).Find(&row)
+	fmt.Printf("%+v\n", row)
+	fmt.Println("----")
+	return row
 }
