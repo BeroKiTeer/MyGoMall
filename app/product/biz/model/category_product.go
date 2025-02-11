fb7c3216ddb202bb0e2986dd90fcd524bb09c2dd
kklmars <lk123.com>
2025-02-11 19:57:46 +0800

    完善添加商品的逻辑

--- app/product/biz/model/category_product.go
+++ app/product/biz/model/category_product.go
@@ -1,5 +1,7 @@
 package model
 
+import "gorm.io/gorm"
+
 type CategoryProduct struct {
 	Id         int64 `gorm:"primary_key;column:id"`
 	CategoryId int64 `gorm:"column:category_id"`
@@ -8,4 +10,9 @@ type CategoryProduct struct {
 
 func (CategoryProduct) TableName() string {
 	return "category_product"
+}
+
+// 添加商品和分类
+func CreateCPRelation(db *gorm.DB, cp *CategoryProduct) error {
+	return db.Create(cp).Error
 }
--- app/product/biz/service/create_product_test.go
+++ app/product/biz/service/create_product_test.go
@@ -2,20 +2,31 @@ package service
 
 import (
 	"context"
+	"product/biz/dal/mysql"
 	product "product/kitex_gen/product"
 	"testing"
 )
 
 func TestCreateProduct_Run(t *testing.T) {
+	mysql.Init()
 	ctx := context.Background()
 	s := NewCreateProductService(ctx)
 	// init req and assert value
-
-	req := &product.CreateProductReq{}
+	newProduct := &product.Product{
+		Categories:    []string{"1", "2"},
+		Name:          "1",
+		Description:   "1",
+		Price:         float32(2),
+		OriginalPrice: float32(1),
+		Stock:         uint32(1),
+		Images:        "1",
+		Status:        uint32(1),
+	}
+	req := &product.CreateProductReq{
+		Product: newProduct,
+	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
-
-	// todo: edit your unit test
 
 }
