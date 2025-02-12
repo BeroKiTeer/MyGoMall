515166e8c19baae129c03292ef15e1f969040eb1
BeroKiTeer <1377433202@qq.com>
2025-02-12 16:27:22 +0800

    添加购物车商品流程示例

--- app/cart/biz/service/add_item.go
+++ app/cart/biz/service/add_item.go
@@ -15,6 +15,17 @@ func NewAddItemService(ctx context.Context) *AddItemService {
 // Run create note info
 func (s *AddItemService) Run(req *cart.AddItemReq) (resp *cart.AddItemResp, err error) {
 	// Finish your business logic.
+	// TODO: 1.参数验证
+
+	// TODO: 2.检查购物车是否已经存在
+
+	// TODO: 3.检查商品是否存在（RPC）
+
+	// TODO: 4.检查商品库存是否足够（可选）
+
+	// TODO: 5.将商品添加到购物车
+
+	// TODO: 6.持久化存储
 
 	return
 }
