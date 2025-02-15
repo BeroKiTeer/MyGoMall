02d49e9b19b24cf9126fd734312b2819180537c5
Blaze <1095661728@qq.com>
2025-02-15 14:35:25 +0800

    编写了userLogin

--- apis/biz/handler/user/user_service.go
+++ apis/biz/handler/user/user_service.go
@@ -110,9 +110,20 @@ func UserLogin(ctx context.Context, c *app.RequestContext) {
 		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
 		return
 	}
-
-	resp := new(user.UserLoginResp)
-
+	//将请求体的json格式绑定到req这一结构体中(即将hjson中的value赋值到req对应的字段)
+	if err := c.Bind(&req); err != nil {
+		c.JSON(400, map[string]string{"error": "Invalid request body"})
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+	resp, err := rpc.UserClient.Login(ctx, &user_kitex.LoginReq{
+		Email:    req.Email,
+		Password: req.Password,
+	})
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
 	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
 }
 
