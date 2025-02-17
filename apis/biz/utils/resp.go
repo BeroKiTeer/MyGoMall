8572b12aa382dc43f702dbed5e207171c8aa5ad1
BeroKiTeer <1377433202@qq.com>
2025-02-17 14:39:15 +0800

    格式化返回前端的JSON

--- apis/biz/utils/resp.go
+++ apis/biz/utils/resp.go
@@ -10,7 +10,12 @@ import (
 // SendErrResponse  pack error response
 func SendErrResponse(ctx context.Context, c *app.RequestContext, code int, err error) {
 	// todo edit custom code
-	c.String(code, err.Error())
+	data := map[string]interface{}{
+		"code":    code,
+		"message": consts.StatusMessage(code),
+		"data":    err,
+	}
+	c.JSON(code, data)
 }
 
 // SendSuccessResponse  pack success response
