0851878676deb6b0c39f60b2c87f4f22f7d576a4
BeroKiTeer <1377433202@qq.com>
2025-02-18 17:32:04 +0800

    新增订单接口大体流程框架

--- app/order/biz/service/place_order.go
+++ app/order/biz/service/place_order.go
@@ -15,6 +15,13 @@ func NewPlaceOrderService(ctx context.Context) *PlaceOrderService {
 // Run create note info
 func (s *PlaceOrderService) Run(req *order.PlaceOrderReq) (resp *order.PlaceOrderResp, err error) {
 	// Finish your business logic.
+	// TODO: 1. 验证token的有效性
+
+	// TODO: 2. 验证库存
+
+	// TODO: 3. 计算订单价格
+
+	// TODO: 4. 创建订单
 
 	return
 }
