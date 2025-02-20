f27e442b20421999d9816f464252e30029617626
kklmars <lk123.com>
2025-02-20 19:16:37 +0800

    将price字段由float替换为int64--proto+model部分

--- app/product/biz/model/product.go
+++ app/product/biz/model/product.go
@@ -9,7 +9,7 @@ type Product struct {
 	Base
 	Name          string  `gorm:"column:name"`
 	Description   string  `gorm:"column:description"`
-	Price         float32 `gorm:"column:price"`
+	Price         int64   `gorm:"column:price"`
 	OriginalPrice float32 `gorm:"column:original_price"`
 	Images        string  `gorm:"column:images"`
 	Stock         uint32  `gorm:"column:stock"`
@@ -18,11 +18,6 @@ type Product struct {
 
 func (p Product) TableName() string {
 	return "products"
-}
-
-// CreateProduct 添加商品
-func CreateProduct(db *gorm.DB, product *Product) error {
-	return db.Create(product).Error
 }
 
 func GetProduct(db *gorm.DB, id int) (Product, error) {
--- app/product/kitex_gen/product/product.pb.fast.go
+++ app/product/kitex_gen/product/product.pb.fast.go
@@ -88,7 +88,7 @@ func (x *Product) fastReadField3(buf []byte, _type int8) (offset int, err error)
 }
 
 func (x *Product) fastReadField4(buf []byte, _type int8) (offset int, err error) {
-	x.Price, offset, err = fastpb.ReadFloat(buf, _type)
+	x.Price, offset, err = fastpb.ReadInt64(buf, _type)
 	return offset, err
 }
 
@@ -595,7 +595,7 @@ func (x *Product) fastWriteField4(buf []byte) (offset int) {
 	if x.Price == 0 {
 		return offset
 	}
-	offset += fastpb.WriteFloat(buf[offset:], 4, x.GetPrice())
+	offset += fastpb.WriteInt64(buf[offset:], 4, x.GetPrice())
 	return offset
 }
 
@@ -956,7 +956,7 @@ func (x *Product) sizeField4() (n int) {
 	if x.Price == 0 {
 		return n
 	}
-	n += fastpb.SizeFloat(4, x.GetPrice())
+	n += fastpb.SizeInt64(4, x.GetPrice())
 	return n
 }
 
