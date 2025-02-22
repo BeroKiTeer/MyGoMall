274c6940da46e57cabbd933e291ffa5f58d9ca9f
Blaze <1095661728@qq.com>
2025-02-22 14:40:26 +0800

    网关层添加order的rpc服务

--- apis/rpc/init.go
+++ apis/rpc/init.go
@@ -12,6 +12,6 @@ func InitClient() {
 		initUserClient()
 		initProductClient()
 		initCartClient()
-
+		initOrderClient()
 	})
 }
---
+++ apis/rpc/order.go
@@ -0,0 +1,25 @@
+package rpc
+
+import (
+	"github.com/BeroKiTeer/MyGoMall/common/kitex_gen/cart/cartservice"
+	"github.com/cloudwego/kitex/client"
+	consul "github.com/kitex-contrib/registry-consul"
+	"order/conf"
+)
+
+var (
+	OrderClient cartservice.Client
+)
+
+func initOrderClient() {
+	var opts []client.Option
+	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		panic(err)
+	}
+	opts = append(opts, client.WithResolver(r))
+	CartClient, err = cartservice.NewClient("order", opts...)
+	if err != nil {
+		panic(err)
+	}
+}
