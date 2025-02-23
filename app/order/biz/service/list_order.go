dd61315331fcb34a8c00cb5fb8199fffb9003b8e
zxy <1422020112@qq.com>
2025-02-23 15:59:30 +0800

    小改动

--- app/order/biz/service/list_order.go
+++ app/order/biz/service/list_order.go
@@ -30,9 +30,7 @@ func (s *ListOrderService) Run(req *order.ListOrderReq) (resp *order.ListOrderRe
 				StreetAddress:   item.ShippingAddress,
 				Name:            item.RecipientName,
 			},
-			OrderItems: &order.OrderItem{
-				//未完待续
-			},
+			OrderItems: &order.OrderItem{},
 		}
 		resp.Orders = append(resp.Orders, ord)
 	}
