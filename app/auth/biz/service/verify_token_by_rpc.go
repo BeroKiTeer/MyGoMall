18f57d1ffb9c563aea0d1d7ef6cc6e8c3b591daa
BeroKiTeer <1377433202@qq.com>
2025-03-02 13:13:26 +0800

    加点日志

--- app/auth/biz/service/verify_token_by_rpc.go
+++ app/auth/biz/service/verify_token_by_rpc.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/BeroKiTeer/MyGoMall/common/kitex_gen/auth"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/golang-jwt/jwt/v4"
 	"strconv"
 )
@@ -21,6 +22,7 @@ func (s *VerifyTokenByRPCService) Run(req *auth.VerifyTokenReq) (resp *auth.Veri
 
 	userID, err := GetUserIDFromToken(req.Token)
 	if err != nil {
+		klog.Error("获取token失败", err)
 		return nil, err
 	}
 
@@ -34,6 +36,7 @@ func (s *VerifyTokenByRPCService) Run(req *auth.VerifyTokenReq) (resp *auth.Veri
 	// 按照查询到的密钥，解析并验证 Token
 	token, err := jwt.Parse(req.Token, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			klog.Errorf("不支持的签名方法: %v", token.Header["alg"])
 			return nil, fmt.Errorf("不支持的签名方法: %v", token.Header["alg"])
 		}
 		// 密钥只支持 []byte 类型，强转。
@@ -46,6 +49,7 @@ func (s *VerifyTokenByRPCService) Run(req *auth.VerifyTokenReq) (resp *auth.Veri
 
 	// 断言解析结果，返回是否对得上
 	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		klog.Info("身份验证成功")
 		return &auth.VerifyResp{Res: true}, nil
 	} else {
 		return &auth.VerifyResp{Res: false}, nil
