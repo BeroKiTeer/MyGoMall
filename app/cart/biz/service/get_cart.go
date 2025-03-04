6e97d5d2742dad8d16b122141b1ba3312ddfc48d
BeroKiTeer <1377433202@qq.com>
2025-03-04 11:34:07 +0800

    支持Redis集群模式

--- app/cart/biz/service/get_cart.go
+++ app/cart/biz/service/get_cart.go
@@ -3,6 +3,7 @@ package service
 import (
 	"cart/biz/dal/redis"
 	"cart/biz/model"
+	"cart/conf"
 	"context"
 	"errors"
 	"fmt"
@@ -29,52 +30,101 @@ func (s *GetCartService) Run(req *cart.GetCartReq) (resp *cart.GetCartResp, err 
 		klog.Error("未输入用户id", err)
 		return nil, errors.New("user id is required")
 	}
-
-	// 看看 redis 里面有没有这个用户的购物车
-	key := fmt.Sprintf("cart:%d", req.UserId)
-	exist, err := redis.RedisClient.Exists(s.ctx, key).Result()
 	var userCart cart.Cart
+	if conf.GetEnv() == "test" {
+		// 看看 redis 里面有没有这个用户的购物车
+		key := fmt.Sprintf("cart:%d", req.UserId)
+		exist, err := redis.RedisClient.Exists(s.ctx, key).Result()
 
-	// redis 里面有，尝试从 redis 中查找
-	if exist == 1 {
-		query, terr := redis.RedisClient.LRange(s.ctx, key, 0, -1).Result()
-		err = terr
-		for _, v := range query {
-			str := strings.Split(v, ":")
-			productID, terr := strconv.Atoi(str[0])
-			quantity, terr := strconv.Atoi(str[1])
-			if terr != nil {
-				err = terr
-				break
-			}
-			userCart.Items = append(userCart.Items, &cart.CartItem{ProductId: int64(productID), Quantity: int32(quantity)})
-		}
-	}
-
-	// redis 里面没有，或者查找发生错误，从 mysql 中查找
-	if exist == 0 || err != nil {
-		err = nil
-		userCart.UserId = req.UserId
-		err = model.QueryItemsByUser(&userCart)
-
-		if err != nil {
-			return nil, errors.New("购物车数据查询时出现错误")
-		}
-
-		// 存进 redis
-		for _, item := range userCart.Items {
-			value := fmt.Sprintf("%d:%d", item.ProductId, item.Quantity)
-			err = redis.RedisClient.LPush(s.ctx, key, value).Err()
-			if err != nil {
-				return nil, err
+		// redis 里面有，尝试从 redis 中查找
+		if exist == 1 {
+			query, terr := redis.RedisClient.LRange(s.ctx, key, 0, -1).Result()
+			err = terr
+			for _, v := range query {
+				str := strings.Split(v, ":")
+				productID, terr := strconv.Atoi(str[0])
+				quantity, terr := strconv.Atoi(str[1])
+				if terr != nil {
+					err = terr
+					break
+				}
+				userCart.Items = append(userCart.Items, &cart.CartItem{ProductId: int64(productID), Quantity: int32(quantity)})
 			}
 		}
-		err = redis.RedisClient.Expire(s.ctx, key, 3600*6*time.Second).Err()
-		// 有 err 说明 redis 坏了，撤销缓存操作
-		if err != nil {
+
+		// redis 里面没有，或者查找发生错误，从 mysql 中查找
+		if exist == 0 || err != nil {
 			err = nil
-			redis.RedisClient.Del(s.ctx, key)
+			userCart.UserId = req.UserId
+			err = model.QueryItemsByUser(&userCart)
+
+			if err != nil {
+				return nil, errors.New("购物车数据查询时出现错误")
+			}
+
+			// 存进 redis
+			for _, item := range userCart.Items {
+				value := fmt.Sprintf("%d:%d", item.ProductId, item.Quantity)
+				err = redis.RedisClient.LPush(s.ctx, key, value).Err()
+				if err != nil {
+					return nil, err
+				}
+			}
+			err = redis.RedisClient.Expire(s.ctx, key, 3600*6*time.Second).Err()
+			// 有 err 说明 redis 坏了，撤销缓存操作
+			if err != nil {
+				err = nil
+				redis.RedisClient.Del(s.ctx, key)
+			}
 		}
+
+	} else if conf.GetEnv() == "dev" {
+		// 看看 redis 里面有没有这个用户的购物车
+		key := fmt.Sprintf("cart:%d", req.UserId)
+		exist, err := redis.RedisClusterClient.Exists(s.ctx, key).Result()
+
+		// redis 里面有，尝试从 redis 中查找
+		if exist == 1 {
+			query, terr := redis.RedisClusterClient.LRange(s.ctx, key, 0, -1).Result()
+			err = terr
+			for _, v := range query {
+				str := strings.Split(v, ":")
+				productID, terr := strconv.Atoi(str[0])
+				quantity, terr := strconv.Atoi(str[1])
+				if terr != nil {
+					err = terr
+					break
+				}
+				userCart.Items = append(userCart.Items, &cart.CartItem{ProductId: int64(productID), Quantity: int32(quantity)})
+			}
+		}
+
+		// redis 里面没有，或者查找发生错误，从 mysql 中查找
+		if exist == 0 || err != nil {
+			err = nil
+			userCart.UserId = req.UserId
+			err = model.QueryItemsByUser(&userCart)
+
+			if err != nil {
+				return nil, errors.New("购物车数据查询时出现错误")
+			}
+
+			// 存进 redis
+			for _, item := range userCart.Items {
+				value := fmt.Sprintf("%d:%d", item.ProductId, item.Quantity)
+				err = redis.RedisClusterClient.LPush(s.ctx, key, value).Err()
+				if err != nil {
+					return nil, err
+				}
+			}
+			err = redis.RedisClusterClient.Expire(s.ctx, key, 3600*6*time.Second).Err()
+			// 有 err 说明 redis 坏了，撤销缓存操作
+			if err != nil {
+				err = nil
+				redis.RedisClusterClient.Del(s.ctx, key)
+			}
+		}
+
 	}
 
 	log.Println(err)
